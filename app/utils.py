af8115a409a8d9c13a50b2973e960acf520609eb
TarunAdobe <ttomar@adobe.com>
2023-10-02 09:12:56 +0530

    feat: added setup.py for building python package

--- app/utils.py
+++ app/utils.py
@@ -15,35 +15,23 @@ def init_nl(args):
     # Define the path to the template inference.py file
     inference_path = os.path.join(os.path.dirname(
         __file__), 'templates', 'inference.py')
-    config_path = os.path.join(os.path.dirname(
-        __file__), 'templates', 'config.yaml')
 
     # Get the current working directory
     root_dir = os.getcwd()
 
     # Define the path where you want to copy the template file
     destination_path_inference = os.path.join(root_dir, 'inference.py')
-    config_path_inference = os.path.join(root_dir, 'config.yaml')
 
     # Copy the template file to the current directory
     shutil.copyfile(inference_path, destination_path_inference)
-    shutil.copyfile(config_path, config_path_inference)
 
-    new_data = {
-        'project_name': project_name
+    data = {
+        'project_name': project_name,
+        'inference_file': './inference.py'
     }
 
-    # Read the existing data from the YAML file
-    existing_data = {}
-
-    with open('config.yaml', 'r') as yaml_file:
-        # Initialize as an empty dictionary if the file is empty
-        existing_data = yaml.safe_load(yaml_file) or {}
-
-    existing_data.update(new_data)
-
     with open('config.yaml', 'w') as yaml_file:
-        yaml.dump(existing_data, yaml_file, default_flow_style=False)
+        yaml.dump(data, yaml_file, default_flow_style=False)
 
     print("initialized neura-launch project")
 
